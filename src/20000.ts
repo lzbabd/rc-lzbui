0d212a8db4de4e49e1cd2a73a2c8b4a46366293c
Murphite <lzbabd@gmail.com>
2024-03-07 11:56:06 +0800

    pkg

---
+++ src/20000.ts
@@ -0,0 +1,73 @@
+
+/*
+一. Ambul=walk,表示“行走， 走路”
+词根助记： ambulance 救护车， 流动医院
+备注： ambulance 这个单词最原始的意思表示的是战地医院。 人抬着单价到处跑， 看看哪
+有伤员。 所以 ambulance 这个单词里面就有个 ambul- 表示行走， 走路的意思。
+照顾基础差同学： 以防班级万一有同学不认识 ambulance,你可以通过谐音记忆“俺不能死”
+记住这个单词。 然后再进行下面其他单词的记忆。
+1. Ambulant adj.走动的， 流动的
+1） 词根： ambul- 词根表示走
+2） 后缀： －ant 形容词后缀或者名词后缀， 比如 important, 比如 accountant 会计,
+  比如 assistant 助理
+2. circumambulate v. 环行， 绕…而行
+1） 前缀： circum-表示圆圈；
+比如 circumstance 环境； circumvent 回避（circum- + vent-词根表示走）
+2） 词根： ambul-表示走
+3） 后缀： -ate 动词或者形容词后缀
+3. perambulate v.游历， 巡游
+1) 前缀： per- 到处， 从头到位， 从始至终； 如 perfect 完美——per + fect(词根表示做)
+2) 词根: ambul-表示走；
+3) 后缀： －ate 表示动词或者形容词后缀；
+比如 private 私下的； considerate 考虑周全的； accelerate 加速
+4. somnambulate /v. 梦游,梦行（额外扩展）
+1） 词根： somn- 表示睡觉；
+比如 insomnia 失眠（in- 表示否定＋somn 词根表示睡觉＋ia 名词后缀）
+2） 词根： ambul- 表示行走
+3） 后缀： －ate 动词后缀或者形容词后缀
+二. Anim=life,spirit, 表示“生命， 精神”等
+词根助记： animal 动物
+备注： 动物就是造物主创造的一种有生命的东西。 所以用 anim- 生命＋ al 名词后缀或者
+形容词后缀
+1. animate /v.使有生气， 活泼
+1) 词根： anim- 表示生命
+2) 后缀： -ate 表示动词后缀或者形容词后缀
+2. animation n/n. 生动； 动画
+1) 词根： anim-表示生命
+2 后缀： －ation 名词后缀
+3. inanimate adj. 无生命的
+1) 前缀： in- 表示否定或者向内
+2) 词根： anim- 表示生命
+3) 后缀： －ate 动词后者形容词后缀
+4. magnanimous adj.心胸博大的
+1) 前缀： magn- 表示大；
+比如： magnify 放大 magn-表示大＋-ify 动词后缀； 再比如： classify
+2) 词根： anim- 表示生命
+3) 后缀： －ous 形容词后缀， 有多的含义
+5. unanimous adj. 意见一致的
+1) 前缀： un 此处表示的是 uni- 表示单一， 比如 unit 单元； USA
+2) 词根： anim-表示生命
+3) 后缀： －ous 表示形容词后缀
+6. animosity n. 憎恨， 憎恶
+1） 词根： anim- 表示生命
+2） 后缀： -osity 复合后缀表名词。 其中－ous 形容词后缀， －ity 名词后缀， 比如
+acidity, stupidity.
+  练习： ——如何建立练习。
+7. equanimity n.平静； 沉着 (额外扩展)
+1） 词根： equ-表示相等；
+2） 词根： anim-表示生命
+3） 后缀： －ity 名词后缀
+重点单词
+
+stupidity	[stju:'piditi]	n. 愚蠢
+circumvent	[.sə:kəm'vent]	vt. 围住， 绕行， 用计谋应付
+animosity	[.æni'mɔsiti]	n. 憎恶， 仇恨
+unanimous	[ju'næniməs]	adj. 全体一致的， 一致同意的
+insomnia	[in'sɔmniə]	n. 失眠(症)
+equanimity	[.i:kwə'nimiti]	n. 平静， 镇定
+animation	[.æni'meiʃən]	n. 活泼， 有生气， 卡通制作
+acidity	[ə'siditi]	n. 酸性， 酸度
+inanimate	[in'ænimit]	adj. 无生命的
+magnanimous	[mæg'næniməs]	[mæg'næniməs]
+
+*/
